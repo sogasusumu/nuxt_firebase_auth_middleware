d5c387275114c7f08c4a074792d3f83953fb7702
曽我進 <soga@yocto-inc.com>
2019-06-06 18:19:26 +0900

    add envfile.

---
+++ plugins/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseUrl: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+  })
+}
+
+export default firebase
